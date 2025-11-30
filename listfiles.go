164eafbb72063a8043d8b6d24fb87e40beb729a0
GypsyCosmonaut <rahulbansal1913@gmail.com>
2025-11-30 22:59:04 +0000

    updated

--- listfiles.go
+++ listfiles.go
@@ -1,18 +1,34 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
+	"time"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		log.Fatalf("Usage: %s <directory-path>", os.Args[0])
-	}
+	showAll := flag.Bool("a", false, "show hidden files")
+	showModTime := flag.Bool("m", false, "show modification time")
+	flag.Parse()
 
-	dirPath := os.Args[1]
+	// Determine directory path
+	var dirPath string
+
+	// If user passed a directory argument, use it
+	if flag.NArg() > 0 {
+		dirPath = flag.Arg(0)
+	} else {
+		// Otherwise use current working directory
+		wd, err := os.Getwd()
+		if err != nil {
+			log.Fatalf("Failed to get working directory: %v", err)
+		}
+		dirPath = wd
+	}
 
 	entries, err := os.ReadDir(dirPath)
 	if err != nil {
@@ -20,8 +36,27 @@ func main() {
 	}
 
 	for _, entry := range entries {
-		fullPath := filepath.Join(dirPath, entry.Name())
-		fmt.Println(fullPath)
+		name := entry.Name()
+
+		// Skip hidden files if -a is not set
+		if !*showAll && strings.HasPrefix(name, ".") {
+			continue
+		}
+
+		fullPath := filepath.Join(dirPath, name)
+
+		// If -m, fetch file info for mod time
+		if *showModTime {
+			info, err := entry.Info()
+			if err != nil {
+				fmt.Printf("%s (error reading info)\n", fullPath)
+				continue
+			}
+			mod := info.ModTime().Format(time.RFC3339)
+			fmt.Printf("%s  %s\n", fullPath, mod)
+		} else {
+			fmt.Println(fullPath)
+		}
 	}
 }
 
